07467d68792eec358d0cbd696a209bd766419656
komalshinde07-ui <komal.shindesk07@gmail.com>
2026-05-27 09:10:13 +0530

    Initial commit

---
+++ src/main/java/com/naukri/automation/Config.java
@@ -0,0 +1,16 @@
+package com.naukri.automation;
+public class Config {
+
+    public static final String USERNAME = "your_email";
+    public static final String PASSWORD = "your_password";
+    public static final String MOBILE = "9876543210";
+
+    public static final String RESUME_PATH =
+            "C:\\Resume\\Resume.pdf";
+
+    public static final String LOGIN_URL =
+            "https://www.naukri.com/nlogin/login";
+
+    public static final String PROFILE_URL =
+            "https://www.naukri.com/mnjuser/profile";
+}
---
+++ src/main/java/com/naukri/automation/NaukriUpdate.java
@@ -0,0 +1,113 @@
+package com.naukri.automation;
+import java.time.Duration;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class NaukriUpdate {
+
+    static WebDriver driver;
+    static WebDriverWait wait;
+
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        try {
+
+            login();
+
+            updateProfile();
+
+            uploadResume();
+
+            logout();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+
+    public static void login() {
+
+        driver.get(Config.LOGIN_URL);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("usernameField"))).sendKeys(Config.USERNAME);
+
+        driver.findElement(By.id("passwordField"))
+                .sendKeys(Config.PASSWORD);
+
+        driver.findElement(
+                By.xpath("//*[@type='submit' and normalize-space()='Login']"))
+                .click();
+
+        System.out.println("Login Successful");
+    }
+
+    public static void updateProfile() throws Exception {
+
+        Thread.sleep(5000);
+
+        driver.findElement(
+                By.xpath("//*[contains(@class,'view-profile')]//a"))
+                .click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(
+                By.xpath("(//*[contains(@class,'icon edit')])[1]"))
+                .click();
+
+        WebElement mobile =
+                wait.until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//*[@name='mobile']")));
+
+        mobile.clear();
+        mobile.sendKeys(Config.MOBILE);
+
+        driver.findElement(
+                By.xpath("//button[@type='submit']"))
+                .click();
+
+        System.out.println("Profile Updated");
+    }
+
+    public static void uploadResume() throws Exception {
+
+        driver.get(Config.PROFILE_URL);
+
+        Thread.sleep(3000);
+
+        WebElement upload =
+                wait.until(ExpectedConditions.presenceOfElementLocated(
+                        By.id("attachCV")));
+
+        upload.sendKeys(Config.RESUME_PATH);
+
+        System.out.println("Resume Uploaded");
+    }
+
+    public static void logout() {
+
+        try {
+
+            driver.findElement(
+                    By.xpath("//a[@data-type='logoutLink']"))
+                    .click();
+
+            System.out.println("Logout Successful");
+
+        } catch (Exception e) {
+            System.out.println("Logout Failed");
+        }
+    }
+}
